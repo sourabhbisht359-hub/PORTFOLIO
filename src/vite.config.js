5b8adb7ced869f4dae8b2efe8616e37a2093f4e5
Sourabh Bisht <sourabhbisht359@gmail.com>
2025-12-29 18:35:25 +0530

    Update vite.config.js

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,11 +1,1 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  preview: {
-    allowedHosts: [
-      "preview.allowedHosts"
-    ]
-  }
-});
