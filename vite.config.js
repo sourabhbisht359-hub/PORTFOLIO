bcd07514b3a90f5437897a314859dc05cf8936a3
Sourabh Bisht <sourabhbisht359@gmail.com>
2025-12-29 19:00:28 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
+  base: "/",   // ✅ VERY IMPORTANT (fixes blank page)
+
   preview: {
     allowedHosts: [
       "portfolio-9urm.onrender.com"
